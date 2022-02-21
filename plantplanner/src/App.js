43ce44e6f80822d990479496b63ac151a71c248d
EdvardJ <eetu.jacobson@gmail.com>
2022-02-21 12:50:36 +0200

    Adds React -includes static plantcard

---
+++ plantplanner/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+export default function App() {
+  
+  return (
+<div class="PlantCard">
+  <div id="imgDiv">
+    <img alt="plantImg"></img>
+    <h2>Kasvin nimi</h2>
+    </div>
+        <div id="textField">
+<p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </div>
+      <div id="indicatorCont">
+<div class="indicator">Kasteltu viimeksi:<div class="colorInd" id="color1"></div></div>
+<div class="indicator">Lannoitettu viimeksi:<div class="colorInd" id="color2"></div></div>
+<div class="indicator">Jotain ja jotain:<div class="colorInd" id="color3"></div></div>
+<div class="indicator">Jotain ja jotain:<div class="colorInd" id="color4"></div></div>
+      </div>
+
+
+ </div>);
+}
