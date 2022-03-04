acba1bd90687819a7efde8b5b7e3afee1feb15ef
Iiro Kajaniemi <kajaniemi.iiro@gmail.com>
2022-03-04 10:28:54 +0200

    css fixes, formatting

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ export default function App() {
     <p>Haku</p>
     <p>Menu</p>
   </div>
-</header>
+</header> {/* ./page-header */}
 
 <div class="PlantCard">
   <div id="imgDiv">
     <img alt="plantImg"></img>
     <h2>Kasvin nimi</h2>
-  </div>
+  </div> {/* ./imgDiv */}
   <div id="textField">
       <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-  </div>
+  </div> {/* ./textField */}
   <div id="indicatorCont">
     <div class="indicator">Kasteltu viimeksi:<div class="colorInd" id="color1"></div></div>
     <div class="indicator">Lannoitettu viimeksi:<div class="colorInd" id="color2"></div></div>
@@ -42,7 +42,7 @@ export default function App() {
         </a>
     </ul>
   </div>
-</footer>
+</footer> {/* ./page-footer */}
 </>
 );
 }
