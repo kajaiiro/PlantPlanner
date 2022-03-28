d5c8ca0ad20015ac1680202bb5ef3c69e2cd7ad0
EdvardJ <eetu.jacobson@gmail.com>
2022-03-28 11:31:35 +0300

    NewUser + footer fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,28 +2,39 @@ import React from "react";
 
 function About() {
   return (
-    <div className="about">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">About</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+      <div class="home">
+          <div className="create-user">
+          <div class="d-flex justify-content-center">
+          <form className="NewUser">
+          <div class="row">
+            <h1>New User</h1>
+            </div>
+            
+            <div class="row">
+
+      <div class="col-25">
+        <label for="fname">Email</label>
+      </div>
+      <div class="col-75">
+        <input type="text" id="email" name="email" placeholder="Your email.."></input>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+        <label for="lname">Password</label>
+      </div>
+      <div class="col-75s">
+        <input type="text" id="password" name="password" placeholder="Your password"></input>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <input type="submit" value="Submit"></input>
+    </div>
+            </form>
           </div>
         </div>
       </div>
-    </div>
+
   );
 }
 
