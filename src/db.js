54385cb90da7af3cf0c4ecb48b44a3c4f245c1d1
Iiro Kajaniemi <kajaniemi.iiro@gmail.com>
2022-04-07 10:24:44 +0300

    Test function for DB

--- src/db.js
+++ src/db.js
@@ -14,3 +14,27 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB
+
+connectDB()
+
+// Mongoose Schema
+const plantSchema = new mongoose.Schema( {
+    plant_name: String,
+})
+
+// Object model
+const Plant = mongoose.model('Plant', plantSchema)
+
+const Kasvi = new Plant({
+    plant_name: 'Ruusu'
+})
+
+// Function to save plant to DB
+async function saveToDB(name) {
+    await name.save(function (err) {
+        if(err) return console.log(err)
+        console.log(`${name.plant_name} added`)
+    })
+}
+
+saveToDB(Kasvi)
