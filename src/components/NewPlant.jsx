8ff508592cc89f5073954650ba7243cfdb20dd47
Iiro Kajaniemi <kajaniemi.iiro@gmail.com>
2022-04-14 19:40:41 +0300

    typo fix

--- src/components/NewPlant.jsx
+++ src/components/NewPlant.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-const { getInitationTime } = require('../router/timer/timer.js')
+const { getInitationTime } = require('../routes/timer/timer.js')
 
 function NewPlant() {
   return (
