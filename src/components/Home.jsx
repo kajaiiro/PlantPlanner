fee88a23260b081270c1e6f0afc4a5cb35e8b210
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2022-05-12 12:21:39 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,22 +3,23 @@ import React from "react";
 function Home() {
   return (
     //Wrapper
-<div className="home">
-
-    <div className="introduction">
-  <h1>Introduction:</h1>
-  <p>We're four front-end web developments students. Goal of this dynamic group project is to learn how to develop a web service as a group and learn its technologies.</p>
-<br/>Technologies<br/>
-<ul>
-<li>HTML 5</li>
-<li>CSS 3</li>
-<li>Javascript</li>
-<li>React</li>
-</ul>
+<div className="home" class="text-center">
+  <div className="introduction">
+    <h1>Introduction:</h1>
+    <p>We are 3 full-stack web developer students from Taitotalo, Helsinki. The goal of this dynamic group project is to learn how to develop a web service as a group and learn about its technologies.</p>
+    <p>Check out our dev blog here: 
+    <a href="https://plantplanner287818493.wordpress.com/"> Plantplanner Dev Blog</a>
+    </p>
+    <h2>Technologies</h2>
+    <ul>
+    <li>HTML 5</li>
+    <li>CSS 3</li>
+    <li>Javascript</li>
+    <li>React</li>
+    <li>MongoDB</li>
+    <li>Bootstrap</li>
+    </ul>
   </div>
-
-
-
 </div> //Wrapper
 );
 }
