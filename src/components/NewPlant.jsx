0e76f2a9aa5ad9589a576c6b854437b4fd8e0c30
DavittBarry <davittbarry333@gmail.com>
2022-05-12 15:35:42 +0300

    d

--- src/components/NewPlant.jsx
+++ src/components/NewPlant.jsx
@@ -1,60 +1,57 @@
-
 import React from "react";
 const { getInitationTime } = require('../routes/timer/timer.js')
 
 function NewPlant() {
   return (
     <div className="NewPlant">
-       <div class="home">
+      <div class="home">
           <div className="create-user">
-          <div class="d-flex justify-content-center">
-          <form className="NewUser">
-          <div class="row">
-            <h1>New Plant</h1>
-            </div>
-            
-            <div class="row">
+            <div class="d-flex justify-content-center">
+              <form className="NewUser">
 
-      <div class="col-25">
-        <label for="Plantname">Plantname:</label>
-      </div>
-      <div class="col-75">
-        <input type="text" id="Plantname" name="Plantname" placeholder="Your plants name.."></input>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-25">
-        <label for="info">Additional info:</label>
-      </div>
-      <div class="col-75s">
-      <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-      </div>
-    </div>
-    <div class="row">
-    <div class="col-50">
-        <label for="Watering cycle">Watering cycle:</label>
-      </div>
-    <div class="col-50">
-        <select id="country" name="country">
-          <option value="week">Once a week</option>
-          <option value="2week">Every two weeks</option>
-          <option value="month">Once a month</option>
-        </select>
-      </div>
-      </div>
+                <div class="row justify-content-center">
+                  <h1>New Plant</h1>
+                </div>
 
-      <div class="col-50">
-      <p>Plant watered<br/><span id="js-initiation-time">-</span></p>
-          <button type="button" onClick={getInitationTime}>Water</button>
-      </div>
+                <div class="row">
+                  <div class="col">
+                    <label for="Plantname">Plant name:</label>
+                    <input type="text" id="Plantname" name="Plantname" placeholder="Your plant's name..."></input>
+                  </div>
+
+                </div>
+                <div class="row">
+                  <label for="info">Additional info:</label>
+                  <textarea id="subject" name="subject" placeholder="Write something..."></textarea>
+                    
+                </div>
+
+                <div class="row" >
+                  <div class="row-10 ">  
+                    <label for="Watering cycle">Watering cycle:</label>
+                  
+                    <select select id="country" name="country">
+                    <option value="week">Once a week</option>
+                    <option value="2week">Every two weeks</option>
+                    <option value="month">Once a month</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <p>Plant watered: <span id="js-initiation-time"></span></p>
+                  <div class="col">
+                    <button class="col" type="button" onClick={getInitationTime}>Water</button>
+                  </div>
+                </div>
       
-    <div class="row justify-content-center">
-      <input type="submit" value="Submit"></input>
-    </div>
-            </form>
+                <div class="row justify-content-center">
+                <input type="submit" value="Submit"></input>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
       </div>
   );
 }
