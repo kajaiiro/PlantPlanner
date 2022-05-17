6381d635b19848bdcdf99d63e87b191cdb6d324a
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2022-05-17 09:13:06 +0300

    Update Slideshow.jsx

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,42 +1,54 @@
 import React from "react";
 import img1 from "../static_files/img1.jpg";
+import img2 from "../static_files/img2.jpg";
+import img3 from "../static_files/img3.jpg";
 
-/*
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
+
   let i;
   let slides = document.getElementsByClassName("mySlides");
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slideIndex++;
+
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-} */
 
-function Slideshow() {
+  // slides[slideIndex-1].style.display = "block"; // This line somehow makes whole site crash - work in progress.
+
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  
+} 
+
+showSlides();
+
+const Slideshow = () => {
+
   return (
-      
-<div class="container">
-
-  <div class="mySlides">
-    <div class="numbertext"></div>
+  <div class="container">
+  
+    <div class="mySlides">
+      <div class="numbertext"></div>
       <img src={img1} alt="1"></img>
-  </div>
+    </div>
 
-  <div class="mySlides">
-    <div class="numbertext"></div>
-      <img src={img1} alt="2"></img>
-  </div>
+    <div class="mySlides">
+      <div class="numbertext"></div>
+      <img src={img2} alt="2"></img>
+    </div>
 
-  <div class="mySlides">
-    <div class="numbertext"></div>
-      <img src={img1} alt="3"></img>
-  </div>
+    <div class="mySlides">
+      <div class="numbertext"></div>
+      <img src={img3} alt="3"></img>
+    </div>
+    
   </div> 
+  
   );
 }
 
